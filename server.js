9bde0f7c6176da2d5a2dea0bd8bc30d4af4b6a69
Ephraimdev <aigbefoephraim@yahoo.com>
2020-01-04 14:52:05 +0100

    removed initial route

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json('Welcome to Store Contacts'));
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
